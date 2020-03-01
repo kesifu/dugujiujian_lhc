7646052fa95e2f297b7402301bb3da4d39718e2b
fox <m1607890768@gmail.com>
2020-03-01 22:03:24 +0800

    岳不群修改

--- src/main/java/com/atguigu/jianfa/dugujiujian.java
+++ src/main/java/com/atguigu/jianfa/dugujiujian.java
@@ -4,5 +4,9 @@ public class dugujiujian {
 
     public static void main(String[] args) {
         System.out.println("jianfa dugujiujian!!!");
+        System.out.println("jianfa dugujiujian!!!");
+        System.out.println("jianfa dugujiujian!!!");
+        System.out.println("jianfa dugujiujian!!!");
+        System.out.println("jianfa dugujiujian!!!");
     }
 }
