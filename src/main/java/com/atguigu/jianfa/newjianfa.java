3486451df7c3e85544e446e961eeb90267754eaf
kesifu <kesifu54@gmail.com>
2020-03-02 08:21:07 +0800

    新剑法2

--- src/main/java/com/atguigu/jianfa/newjianfa.java
+++ src/main/java/com/atguigu/jianfa/newjianfa.java
@@ -3,5 +3,10 @@ package com.atguigu.jianfa;
 public class newjianfa {
     public static void main(String[] args) {
         System.out.println("新剑法!");
+        System.out.println("新剑法!");
+        System.out.println("新剑法!");
+        System.out.println("新剑法!");
+        System.out.println("新剑法!");
+        System.out.println("新剑法!");
     }
 }
